0c84c327607b3419a6c24358abeb55cc469fb465
technocrat <public@careaga.net>
2025-06-27 19:11:06 -0700

    revised simple_colorbar.jl

--- src/simple_colorbar.jl
+++ src/simple_colorbar.jl
@@ -1,47 +1,118 @@
 using CairoMakie
-using ColorBrewer
+using ColorSchemes
 
 """
-    simple_colorbar(palette_name::String, n_colors::Int)
-
-Display a discrete vertical colorbar using ColorBrewer palettes.
-
-# Arguments
-- `palette_name::String`: Name of the ColorBrewer palette (e.g., "Greens", "Blues", "Reds")
-- `n_colors::Int`: Number of discrete colors to display, must be in range 1:12
-
-# Examples
-```julia
-simple_colorbar("Greens", 5)
-simple_colorbar("Blues", 9)
-simple_colorbar("RdYlBu", 7)
-
-```
+    continuous_colorbar(; colormap=:viridis, label="Value", size=(400, 300))
+Display a heatmap with a continuous colorbar using the specified colormap.
+- `colormap`: Can be a ColorSchemes symbol (e.g., :viridis) or a custom vector of colors.
 """
-function simple_colorbar(palette_name::String, n_colors::Int)
-    # Get the color palette
-    colors = palette(palette_name, n_colors)
-    
-    # Create figure
-    fig = Figure(size = (400, 300))
-    
-    # Create a simple matrix with discrete values 1 to n_colors
-    data = reshape(1:n_colors, 1, n_colors)
-    
-    # Create heatmap
-    ax = Axis(fig[1, 1])
-    hm = heatmap!(ax, data, colormap = colors, colorrange = (1, n_colors))
-    
-    # Add discrete colorbar
-    colorbar = Colorbar(fig[1, 2], hm, 
-                       ticks = 1:n_colors,
-                       label = palette_name,
-                       vertical = true)
-    
-    # Hide axis elements since we just want the colorbar
-    hidedecorations!(ax)
-    
+function continuous_colorbar(; colormap=:viridis, label="Value", size=(400, 300))
+    # Prepend 'Continuous: ' to label if not already present
+    if !occursin(r"continuous"i, label)
+        label = "Continuous: " * label
+    end
+    fig = Figure(size = size)
+    ax = Axis(fig[1, 1], title = "Map Colors",
+        xticksvisible = false,
+        yticksvisible = false,
+        xticklabelsvisible = false,
+        yticklabelsvisible = false)
+
+    # Get the colors from the colormap
+    if typeof(colormap) <: Symbol
+        colors = get(ColorSchemes.colorschemes, colormap, ColorSchemes.viridis)
+        if typeof(colors) <: Symbol
+            colors = ColorSchemes.colorschemes[colors]
+        end
+    else
+        colors = colormap
+    end
+
+    data = rand(10, 10)
+    hm = heatmap!(ax, data, colormap=colors)
+    Colorbar(fig[1, 2], hm, label=label,
+        ticksvisible = false,
+        ticklabelsvisible = false)
     display(fig)
+    return fig
 end
 
-export simple_colorbar
+"""
+    discrete_colorbar(; colormap=:Blues_9, label="Value", size=(400, 300))
+Display a heatmap with a discrete colorbar using the specified colormap.
+- `colormap`: Can be a ColorSchemes symbol (e.g., :Blues_9) or a custom vector of colors.
+"""
+function discrete_colorbar(; colormap=:Blues_9, label="Value", size=(400, 300))
+    # Prepend 'Discrete: ' to label if not already present
+    if !occursin(r"discrete"i, label)
+        label = "Discrete: " * label
+    end
+    fig = Figure(size = size)
+    ax = Axis(fig[1, 1], title = "Map Colors",
+        xticksvisible = false,
+        yticksvisible = false,
+        xticklabelsvisible = false,
+        yticklabelsvisible = false)
+
+    # Get the colors from the colormap
+    if typeof(colormap) <: Symbol
+        colors = get(ColorSchemes.colorschemes, colormap, ColorSchemes.Blues_9)
+        if typeof(colors) <: Symbol
+            colors = ColorSchemes.colorschemes[colors]
+        end
+    else
+        colors = colormap
+    end
+    n_colors = length(colors)
+
+    data = rand(1:n_colors, 10, 10)
+    hm = heatmap!(ax, data, colormap=colors)
+
+    # Place the colorbar in a new row below the heatmap
+    cb_ax = Axis(fig[2, 1],
+        xticksvisible = false, yticksvisible = false,
+        xticklabelsvisible = false, yticklabelsvisible = false,
+        leftspinevisible = false, rightspinevisible = false,
+        topspinevisible = false, bottomspinevisible = false,
+        backgroundcolor = :transparent, height=40)
+    hidespines!(cb_ax)
+    hidedecorations!(cb_ax)
+    
+    # Draw horizontal color boxes
+    bar_width = 1.0 / n_colors
+    for i in 1:n_colors
+        x_center = (i - 0.5) * bar_width
+        y_center = 0.5
+        x_min = x_center - bar_width / 2
+        x_max = x_center + bar_width / 2
+        y_min = 0.0
+        y_max = 1.0
+        rect = [Point2f(x_min, y_min), Point2f(x_max, y_min),
+                Point2f(x_max, y_max), Point2f(x_min, y_max)]
+        poly!(cb_ax, rect, color=colors[i])
+    end
+    xlims!(cb_ax, 0, 1)
+    ylims!(cb_ax, 0, 1)
+    
+    # Add label in a new row below the colorbar
+    Label(fig[3, 1], label, halign = :center, valign = :top, fontsize=18)
+    display(fig)
+    return fig
+end
+
+# Example usage:
+println("Testing continuous colorbar:")
+continuous_colorbar(colormap=:viridis, label="Viridis")
+
+println("\nTesting discrete colorbar:")
+discrete_colorbar(colormap=:Blues_9, label="Blues")
+
+println("\nTesting custom discrete colorbar:")
+custom_colors = [:red, :orange, :yellow, :green, :blue, :purple]
+discrete_colorbar(colormap=custom_colors, label="Custom Colors")
+
+println("\nTesting continuous with Set2_3:")
+continuous_colorbar(colormap=:Set2_3, label="ColorBlewer Set2_3")
+
+println("\nTesting discrete with Set2_3:")
+discrete_colorbar(colormap=:Set2_3, label="ColorBlewer Set2_3")
